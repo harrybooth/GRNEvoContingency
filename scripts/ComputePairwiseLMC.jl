1a5d314fadf32d51e60509b414ce6a0871a7723b
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2023-03-28 09:29:21 +0100

    lmc script upgrade

--- scripts/ComputePairwiseLMC.jl
+++ scripts/ComputePairwiseLMC.jl
@@ -4,19 +4,27 @@ using DrWatson
 
 # using Plots
 
-using DifferentialEquations
+# using Plots
 using Random
-using Parameters
-using StatsBase
 using Printf
-using Distributed
-using Random
 using JLD
 using LinearAlgebra
+using NearestNeighbors
 using DataInterpolations
+using Clustering
+using ClusterValidityIndices
+using MultivariateStats
+using KernelDensity
+using NetworkLayout
+using TravelingSalesmanExact, GLPK
+
+using Distributed
+
+using Memoization
 using SparseArrays
 
-using ProgressMeter
+using Base.Threads
+using Base.Threads: @spawn
 
 @everywhere include(srcdir("TissueModel_ND.jl"))
 @everywhere include(srcdir("Evolution.jl"))
@@ -30,23 +38,21 @@ using ProgressMeter
 
     itp_ll = InterpolatedLandscapeLean(network_1,network_2,30,grn_parameters,development,fitness_function);
 
-    # next!(progress)
-
     return 0.5*(itp_ll.slice_fitnesses[1] + itp_ll.slice_fitnesses[end] + 2) - minimum(itp_ll.slice_fitnesses .+ 1)
 
 end
 
 #########################################
 
-grn_parameters = DefaultGRNParameters();
+@everywhere grn_parameters = DefaultGRNParameters();
 
-development = DefaultGRNSolver()
+@everywhere development = DefaultGRNSolver()
 
-output_gene = 3
+@everywhere output_gene = 3
 
-n_target_stripe = 1
+@everywhere n_target_stripe = 1
 
-fitness_function = s -> fitness_evaluation(s,x->malt_fitness(x,n_target_stripe),output_gene);
+@everywhere fitness_function = s -> fitness_evaluation(s,x->malt_fitness(x,n_target_stripe),output_gene);
 
 #########################################
 
@@ -54,9 +60,11 @@ evo_traces_cl = load(datadir("sims/repeated_evolution_different_topologies","del
 evo_traces_ff = load(datadir("sims/repeated_evolution_different_topologies","deletion_prob=0.05_max_gen=30000_mut_prob=0.1_n_target_stripe=1_n_traj=2000_noise_cv=0.5_start_network_name=half_right_topology=feed_forward_β=1.0_1000.jld2"))["data"]
 evo_traces_mi = load(datadir("sims/repeated_evolution_different_topologies","deletion_prob=0.05_max_gen=40000_mut_prob=0.1_n_target_stripe=1_n_traj=5000_noise_cv=0.5_start_network_name=half_right_topology=mutual_inh_β=1.0_1000.jld2"))["data"];
 
-gt_cl = GenoTrajectories(evo_traces_cl);
-gt_ff = GenoTrajectories(evo_traces_ff);
-gt_mi = GenoTrajectories(evo_traces_mi);
+n_sub_sample = 500
+
+gt_cl = GenoTrajectories(evo_traces_cl[1:n_sub_sample]);
+gt_ff = GenoTrajectories(evo_traces_ff[1:n_sub_sample]);
+gt_mi = GenoTrajectories(evo_traces_mi[1:n_sub_sample]);
 
 end_networks_cl = map(x->x[:,end],gt_cl.geno_traj);
 end_networks_ff = map(x->x[:,end],gt_ff.geno_traj);
@@ -64,36 +72,36 @@ end_networks_mi = map(x->x[:,end],gt_mi.geno_traj);
 
 all_networks = reduce(vcat,[end_networks_cl,end_networks_ff,end_networks_mi]);
 
+print("loaded data...")
+
 ########################################
 
 n_sample = length(all_networks)
 
-# n_sample = 10
+print(n_sample)
 
 dmat = zeros(n_sample,n_sample)
 
-# p = Progress(Int(0.5*n_sample*n_sample) - n_sample,"Computing pairwise matrix...",50)
+dmat_id = [(i,j) for i in 1:n_sample, j in 1:n_sample if i>j]
 
-@sync for i in 1:n_sample
-    for j in 1:n_sample
-        if i>j
-            @spawn dmat[i,j] = instability(all_networks[i],all_networks[j],grn_parameters,development,fitness_function)
-        end
-    end
+@sync for id in dmat_id
+    @spawn dmat[id...] = instability(all_networks[id[1]],all_networks[id[2]],grn_parameters,development,fitness_function)
 end
+
+print("finished comp...") 
 
 # finish!(p)
 
-for i in 1:n_sample
-    for j in 1:n_sample
-        if i<j
-            dmat[i,j] = dmat[j,i]
-        end
-    end
-end
+# for i in 1:n_sample
+#     for j in 1:n_sample
+#         if i<j
+#             dmat[i,j] = dmat[j,i]
+#         end
+#     end
+# end
 
-for i in 1:n_sample
-    dmat[i,i] = 0.
-end
+# for i in 1:n_sample
+#     dmat[i,i] = 0.
+# end
 
-save(datadir("sims/repeated_evolution_different_topologies","pairwise_instabilities_mi_cl_ff_3000.jld2"),"data",dmat)
+save(datadir("sims/repeated_evolution_different_topologies","pairwise_instabilities_mi_cl_ff_" * string(n_sample) * ".jld2"),"data",dmat)
