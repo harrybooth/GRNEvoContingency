ed5ccc25d5f41c1664104b72eb87ea9cf6e92570
harrybooth <harrybooth@live.com>
2023-05-10 10:40:30 +0100

    remove dynamic axis warping

--- src/DynamicalClustering.jl
+++ src/DynamicalClustering.jl
@@ -1,4 +1,3 @@
-using DynamicAxisWarping
 
 function calculate_dyn_distance(n1,t1,n2,t2,dist,n_steps,save_id,relative)
 
@@ -27,18 +26,18 @@ function calculate_dyn_distance(n1,t1,w_ind_2,dist,n_steps,save_id)
     return evaluate(dist, v1, v2)
 end
 
-function calculate_dyn_dtw(n1,n2,t1,t2,save_id)
+# function calculate_dyn_dtw(n1,n2,t1,t2,save_id)
 
-    w_ind_1 = Individual(n1,t1,grn_parameters,DenseGRNSolver(save_id));
-    w_ind_2 = Individual(n2,t2,grn_parameters,DenseGRNSolver(save_id));
+#     w_ind_1 = Individual(n1,t1,grn_parameters,DenseGRNSolver(save_id));
+#     w_ind_2 = Individual(n2,t2,grn_parameters,DenseGRNSolver(save_id));
 
-    ind1_f = t->w_ind_1.phenotype(t*t1)
-    ind2_f = t->w_ind_2.phenotype(t*t2)
+#     ind1_f = t->w_ind_1.phenotype(t*t1)
+#     ind2_f = t->w_ind_2.phenotype(t*t2)
 
-    cost, ϕ, ψ = gdtw(ind1_f,ind2_f)
+#     cost, ϕ, ψ = gdtw(ind1_f,ind2_f)
 
-    return cost
-end
+#     return cost
+# end
 
 function get_rel_dyn_vector(n1,t1,n_steps,save_id)
 
@@ -66,9 +65,9 @@ function get_dense_dyn_sol(n1,t1,save_id)
 
 end
 
-function calculate_dyn_dtw(ind1_f,ind2_f)
+# function calculate_dyn_dtw(ind1_f,ind2_f)
 
-    cost, ϕ, ψ = gdtw(ind1_f,ind2_f)
+#     cost, ϕ, ψ = gdtw(ind1_f,ind2_f)
 
-    return cost
-end
+#     return cost
+# end
