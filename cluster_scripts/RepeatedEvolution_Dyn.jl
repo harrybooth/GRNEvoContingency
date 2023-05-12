ed3b9fb1909b1defd4d3ba0732325275048495cc
harrybooth <harrybooth@live.com>
2023-05-12 16:47:31 +0100

    bistable repeated, lmc

--- cluster_scripts/RepeatedEvolution_Dyn.jl
+++ cluster_scripts/RepeatedEvolution_Dyn.jl
@@ -48,7 +48,7 @@ end
 @everywhere include(srcdirx("FitnessFunctions.jl"))
 @everywhere include(srcdirx("DynamicalClustering.jl"))
 
-@everywhere all_experiments = ["RepeatedEvolution_MutualInh_Dyn"]
+@everywhere all_experiments = ["RepeatedEvolution_Bistable_Dyn"]
 
 for exp_name in all_experiments
 
