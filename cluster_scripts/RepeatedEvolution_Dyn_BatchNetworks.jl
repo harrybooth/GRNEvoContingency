f871bb49546b8250363fd9f6f11ed5c61c65513c
harrybooth <harrybooth@live.com>
2023-05-20 08:23:28 +0100

    fo

--- cluster_scripts/RepeatedEvolution_Dyn_BatchNetworks.jl
+++ cluster_scripts/RepeatedEvolution_Dyn_BatchNetworks.jl
@@ -48,7 +48,7 @@ end
 @everywhere include(srcdirx("FitnessFunctions.jl"))
 @everywhere include(srcdirx("DynamicalClustering.jl"))
 
-@everywhere all_experiments = ["RepeatedEvolution_MutualInh_Dyn_Batch"]
+@everywhere all_experiments = ["RepeatedEvolution_FrozenOsc_Dyn_Batch"]
 
 for exp_name in all_experiments
 
