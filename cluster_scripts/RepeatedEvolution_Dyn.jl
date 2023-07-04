381254bfb0f188fe7562b2ccb69cc641e17ce2fa
harrybooth <harrybooth@live.com>
2023-07-04 10:24:54 +0100

    single mujt

--- cluster_scripts/RepeatedEvolution_Dyn.jl
+++ cluster_scripts/RepeatedEvolution_Dyn.jl
@@ -48,7 +48,7 @@ end
 @everywhere include(srcdirx("FitnessFunctions.jl"))
 @everywhere include(srcdirx("DynamicalClustering.jl"))
 
-@everywhere all_experiments = ["Experiment_1/RE_Minimal_Activating","Experiment_1/RE_Minimal_Inhibiting"]
+@everywhere all_experiments = ["Experiment_1/RE_Minimal_Activating_Single","Experiment_1/RE_Minimal_Inhibiting_Single"]
 
 for exp_name in all_experiments
 
