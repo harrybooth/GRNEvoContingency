c684e0583195fcb4f7d62de07ee0f24db57f33fb
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2023-10-25 10:24:25 +0100

    evo 1

--- cluster_scripts/RepeatedEvolution_MinDyn.jl
+++ cluster_scripts/RepeatedEvolution_MinDyn.jl
@@ -49,7 +49,7 @@ end
 @everywhere include(srcdirx("DynamicalClustering.jl"))
 @everywhere include(srcdirx("MinimalNetworks.jl"))
 
-@everywhere all_experiments = ["Experiment_1/RE_Minimal_Inhibiting_EvoParam_2","Experiment_1/RE_Minimal_Inhibiting_EvoParam_3"]
+@everywhere all_experiments = ["Experiment_1/RE_Minimal_Inhibiting_EvoParam_1"]
 
 # "Experiment_1/RE_Minimal_Inhibiting_Single"
 
