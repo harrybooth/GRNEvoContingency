7214474c02cc1722e0c6d85cc871461e3eb86a21
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2023-11-07 16:21:18 +0000

    main_exp

--- cluster_scripts/RepeatedEvolution_MinDyn.jl
+++ cluster_scripts/RepeatedEvolution_MinDyn.jl
@@ -49,7 +49,7 @@ end
 @everywhere include(srcdirx("DynamicalClustering.jl"))
 @everywhere include(srcdirx("MinimalNetworks.jl"))
 
-@everywhere all_experiments = ["Experiment_1/RE_Minimal_Inhibiting_Kimaru","Experiment_1/RE_Minimal_Inhibiting_Kimaru_RelFitness","Experiment_1/RE_Minimal_Inhibiting_Kimaru_SP"]
+@everywhere all_experiments = ["Final_Experiments/RE_Minimal_Inhibiting_Main"]
 
 # "Experiment_1/RE_Minimal_Inhibiting_Single"
 
