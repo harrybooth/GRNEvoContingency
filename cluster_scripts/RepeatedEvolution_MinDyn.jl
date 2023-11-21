f44c29743c5afe3546e87a9ee4db855bc9fadf80
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2023-11-21 15:56:10 +0000

    exp

--- cluster_scripts/RepeatedEvolution_MinDyn.jl
+++ cluster_scripts/RepeatedEvolution_MinDyn.jl
@@ -51,7 +51,7 @@ end
 
 # @everywhere all_experiments = ["Final_Experiments/RE_Minimal_Inhibiting_Main","Final_Experiments/RE_Minimal_Inhibiting_Main"]
 
-@everywhere all_experiments = ["Final_Experiments/Variations/RE_Minimal_Inhibiting_Theta_2","Final_Experiments/Variations/RE_Minimal_Inhibiting_SingleFitness"]
+@everywhere all_experiments = ["Final_Experiments/Contingencies/RE_Minimal_Inhibiting_Main_Contingent_1","Final_Experiments/Contingencies/RE_Minimal_Inhibiting_Main_Contingent_2"]
 
 # "Experiment_1/RE_Minimal_Inhibiting_Single"
 
