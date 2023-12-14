44079cb99947eb601c486731a264c23db712575a
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2023-12-14 14:41:16 +0000

    v9

--- src/Evolution.jl
+++ src/Evolution.jl
@@ -654,6 +654,54 @@ function noise_mtype_v1iii(w::Matrix{Float64},mut_op::MutationOperatorNew)
     return new_w, choices, mtype, sizes, true
 end
 
+function noise_mtype_v1iii_sign(w::Matrix{Float64},mut_op::MutationOperatorNew)
+
+    new_w = copy(w)
+
+    n_mut = 0
+
+    while n_mut == 0
+        n_mut = mut_op.n_sample_func()
+    end
+
+    choices = sample(mut_op.mutation_weights,n_mut,replace = false)
+    mtype = []
+    sizes = []
+
+    for index in choices
+
+        if new_w[index] == 0
+            n = rand(Uniform(-mut_op.max_w,mut_op.max_w))
+            new_w[index] = n
+            push!(mtype,:new)
+            push!(sizes,n)
+
+        elseif rand(Exponential(0.1)) > abs(new_w[index])
+            new_w[index] = 0
+
+            push!(mtype,:del)
+        else
+            n = exp(-1*rand(mut_op.noise_distribution))
+
+            if rand() < mut_op.sign_flip_probability
+                new_w[index] = -1*new_w[index]*n
+                push!(sizes,-n)
+            else
+                new_w[index] = new_w[index]*n
+                push!(sizes,n)
+            end
+     
+            if abs(new_w[index]) > mut_op.max_w
+                new_w[index] = sign(new_w[index])*mut_op.max_w
+            end
+
+            push!(mtype,:existing)
+        end
+    end
+
+    return new_w, choices, mtype, sizes, true
+end
+
 
 # Selection 
 
