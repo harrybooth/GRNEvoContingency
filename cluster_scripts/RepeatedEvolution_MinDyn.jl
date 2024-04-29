eab649504870ccf6e6c1beb41afc8344d4004c5c
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2024-04-29 16:13:46 +0100

    single mut small n

--- cluster_scripts/RepeatedEvolution_MinDyn.jl
+++ cluster_scripts/RepeatedEvolution_MinDyn.jl
@@ -55,7 +55,7 @@ end
 
 # @everywhere all_experiments = ["Final_Experiments/RE_Minimal_Inhibiting_Main_MA_NLarge","Final_Experiments/RE_Minimal_Inhibiting_Main_MA_NLarge","Final_Experiments/RE_Minimal_Inhibiting_Main_MA_NLarge","Final_Experiments/RE_Minimal_Inhibiting_Main_MA_NLarge","Final_Experiments/RE_Minimal_Inhibiting_Main_MA_NLarge","Final_Experiments/RE_Minimal_Inhibiting_Main_MA_NLarge"]
 
-all_experiments = ["DeNovoStripe/RE_Minimal_Activating_DeNovo","DeNovoStripe/RE_Minimal_Activating_DeNovo","DeNovoStripe/RE_Minimal_Activating_DeNovo"]
+all_experiments = ["DeNovoStripe/RE_Minimal_Inhibiting_DeNovo_SingleMutation_SmallN"]
 
 for exp_name in all_experiments
 
