77867ae66c00d2c1aa5a7260dba4b98f37b7e07c
Harry Booth <39346323+harrybooth@users.noreply.github.com>
2025-02-04 09:09:15 +0000

    sbatch

--- cluster_scripts/RepeatedEvolution_MinDyn_2.jl
+++ cluster_scripts/RepeatedEvolution_MinDyn_2.jl
@@ -42,7 +42,7 @@ end
 
 @everywhere include(srcdirx("GRNEvoContingency.jl"))
 
-all_experiments = ["DeNovoStripe/AdditionalAnalysis/RE_MinimalInhibiting_IC_2"]
+all_experiments = ["DeNovoStripe/AdditionalAnalysis/RE_MinimalInhibiting_SmallP_1"]
 
 for exp_name in all_experiments
 
